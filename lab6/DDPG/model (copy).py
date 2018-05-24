82c07585773f9c36edbb17b5dbca5cf68b46aaf9
SunnerLi <a6214123@gmail.com>
2018-05-24 16:12:12 +0800

    add a simple version that can perform pendulumn-v0 with success

--- lab6/DDPG/model (copy).py
+++ lab6/DDPG/model (copy).py
@@ -8,34 +8,41 @@ import numpy as np
 import torch
 
 class Actor(nn.Module):
-    def __init__(self, n_state):
+    def __init__(self, n_state, limit, trainable = True):
         super(Actor, self).__init__()
         self.main = nn.Sequential(
-            nn.Linear(n_state, 400),
+            nn.Linear(n_state, 40),
             nn.ReLU(),
-            nn.Linear(400, 300),
+            nn.Linear(40, 30),
             nn.ReLU(),
-            nn.Linear(300, 1),
+            nn.Linear(30, 1),
             nn.Tanh()
         )
+        if not trainable:
+            for param in self.parameters():
+                param.requires_grad = False
+        self.limit = float(limit)
 
     def forward(self, x):
-        return self.main(x)
+        return self.main(x) * self.limit
 
 class Critic(nn.Module):
-    def __init__(self, n_state):
+    def __init__(self, n_state, trainable = True):
         super(Critic, self).__init__()
         self.front = nn.Sequential(
-            nn.Linear(n_state, 400),
+            nn.Linear(n_state, 40),
             nn.ReLU()
         )
         self.end = nn.Sequential(
-            nn.Linear(401, 300),
+            nn.Linear(41, 30),
             nn.ReLU(),
-            nn.Linear(300, 1),
+            nn.Linear(30, 1),
             nn.ReLU(),
             nn.Linear(1, 1)
         )
+        if not trainable:
+            for param in self.parameters():
+                param.requires_grad = False
 
     def forward(self, x, act):
         x = self.front(x)
@@ -43,8 +50,8 @@ class Critic(nn.Module):
         return self.end(x)
 
 class DDPG:
-    def __init__(self, n_state, n_action, a_limit, model_folder, memory_size = 500, 
-                    batch_size = 32, tau = 0.99, gamma = 0.99, 
+    def __init__(self, n_state, n_action, a_limit, model_folder, memory_size = 10000, 
+                    batch_size = 32, tau = 0.01, gamma = 0.9, 
                     epsilon = 50000, is_training = True):
         # Record the parameters
         self.n_state = n_state
@@ -62,14 +69,14 @@ class DDPG:
         # Create the network and related objects
         self.memory = np.zeros([self.memory_size, 2 * self.n_state + self.n_action + 1], dtype = np.float32)
         self.memory_counter = 0
-        self.eval_actor = Actor(self.n_state)
+        self.eval_actor = Actor(self.n_state, a_limit)
         self.eval_critic = Critic(self.n_state)
-        self.target_actor = Actor(self.n_state)
-        self.target_critic = Critic(self.n_state)
+        self.target_actor = Actor(self.n_state, a_limit, trainable = False)
+        self.target_critic = Critic(self.n_state, trainable = False)
         self.hardCopy(self.target_actor, self.eval_actor)
         self.hardCopy(self.target_critic, self.eval_critic)
-        self.actor_optimizer = Adam(self.eval_actor.parameters(), lr = 0.01)
-        self.critic_optimizer = Adam(self.eval_critic.parameters(), lr = 0.02)
+        self.actor_optimizer = Adam(self.eval_actor.parameters(), lr = 0.001)
+        self.critic_optimizer = Adam(self.eval_critic.parameters(), lr = 0.002)
         self.criterion = nn.MSELoss()
         self.random_process = OrnsteinUhlenbeckProcess(size = n_action,
             theta = 0.15, mu = 0.0, sigma = 0.2)
@@ -77,20 +84,32 @@ class DDPG:
     def load(self):
         pass
 
-    def chooseAction(self, s, should_random = False, decay_epsilon = True):
-        """
-            給定輸入state，透過evaluate actor輸出[-1, 1]之間的實數動作值
-        """
-        if not should_random:
-            s = to_var(s)
-            a = self.eval_actor(s)
-            a = a.cpu().data.numpy()
-            a += self.is_training * max(self.epsilon, 0) * self.random_process.sample()
-            a = np.clip(a, -1.0, 1.0)
-            if decay_epsilon:
-                self.epsilon -= self.depsilon
-        else:
-            a = np.random.uniform(-1.0, 1.0, size = self.n_action)
+    # def chooseAction(self, s, should_random = False, decay_epsilon = True):
+    #     """
+    #         給定輸入state，透過evaluate actor輸出[-1, 1]之間的實數動作值
+    #     """
+    #     # if not should_random:
+    #     #     s = to_var(s)
+    #     #     a = self.eval_actor(s)
+    #     #     a = a.cpu().data.numpy()
+    #     #     a += self.is_training * max(self.epsilon, 0) * self.random_process.sample()
+    #     #     a = np.clip(a, -1.0, 1.0)
+    #     #     if decay_epsilon:
+    #     #         self.epsilon -= self.depsilon
+    #     # else:
+    #     #     a = np.random.uniform(-1.0, 1.0, size = self.n_action)
+    #     # return a
+    #     s = to_var(s)
+    #     a = self.eval_actor(s)
+    #     a = a.cpu().data.numpy()
+    #     a = np.clip(np.random.normal(a, self.var), -2, 2)
+    #     if self.memory_counter > self.memory_size:
+    #         self.var *= 0.9995
+    #     return a
+    def chooseAction(self, s):
+        s = to_var(s)
+        a = self.eval_actor(s)
+        a = a.cpu().data.numpy()
         return a
 
     def store_path(self, s, a, r, s_):
@@ -112,16 +131,13 @@ class DDPG:
         for target_param, source_param in zip(target.parameters(), source.parameters()):
             target_param.data.copy_(source_param.data)
 
-    def update(self):
-        # 如果儲存的資訊太少就不更新
-        if self.memory_counter <= self.batch_size:
-            return
-        
+    def update(self):       
+        # 將evaluate network的參數複製進入target network中
+        self.softCopy(self.target_actor, self.eval_actor)
+        self.softCopy(self.target_critic, self.eval_critic)
+
         # 決定輸入的batch data
-        if self.memory_counter > self.memory_size:
-            sample_idx = np.random.choice(self.memory_size, size = self.batch_size)
-        else:
-            sample_idx = np.random.choice(self.memory_counter, size = self.batch_size)                
+        sample_idx = np.random.choice(self.memory_size, size = self.batch_size)           
         batch_data = self.memory[sample_idx, :]
         batch_s = batch_data[:, :self.n_state]
         batch_a = batch_data[:, self.n_state:self.n_state+self.n_action]
@@ -134,25 +150,19 @@ class DDPG:
         batch_r = to_var(batch_r)
         batch_s_ = to_var(batch_s_)
 
-        # 用target network計算target Q值
-        next_q_target = self.target_critic(batch_s_, self.target_actor(batch_s_))
-        next_q_target = Variable(next_q_target.data, volatile = False)
-        q_target = batch_r + self.gamma * next_q_target
 
-        # 更新critic
-        self.critic_optimizer.zero_grad()
-        a = self.eval_actor(batch_s)
-        q_batch = self.eval_critic(batch_s, a.detach())
-        value_loss = self.criterion(q_batch, q_target)
-        value_loss.backward()
-        self.critic_optimizer.step()
-
-        # 更新actor
         self.actor_optimizer.zero_grad()
-        policy_loss = -self.eval_critic(batch_s, a).mean()
-        policy_loss.backward()
+        q = self.eval_critic(batch_s, self.eval_actor(batch_s))
+        a_loss = q.mean()
+        a_loss.backward()
         self.actor_optimizer.step()
 
-        # 將evaluate network的參數複製進入target network中
-        self.softCopy(self.target_actor, self.eval_actor)
-        self.softCopy(self.target_critic, self.eval_critic)
+        self.critic_optimizer.zero_grad()
+        q = self.eval_critic(batch_s, self.eval_actor(batch_s))        
+        q_target = batch_r + 0.9 * self.target_critic(batch_s_, self.target_actor(batch_s_))
+        td_error = self.criterion(q, q_target)
+        td_error.backward()
+        self.critic_optimizer.step()
+
+
+        
--- lab6/DDPG/train.py
+++ lab6/DDPG/train.py
@@ -1,4 +1,4 @@
-from model import DDPG
+from model2 import DDPG
 import numpy as np
 import torch
 import gym
@@ -8,16 +8,17 @@ import gym
 """
 
 # 定義hyper-parameters
-episode = 200
-max_iter = 100
+episode = 500
+max_iter = 200
 model_folder = './model'
+warmup = 20
 
 if __name__ == '__main__':
     # Create environment
     env = gym.make('Pendulum-v0').unwrapped
     n_state = env.observation_space.shape[0]        # 提取state的維度
     n_action = env.action_space.shape[0]            # 提取action的維度
-    a_limit = env.action_space.high                 # 提取action連續動作中，最大的可能數值
+    a_limit = env.action_space.high[0]              # 提取action連續動作中，最大的可能數值
 
     # Create network
     net = DDPG(
@@ -26,19 +27,26 @@ if __name__ == '__main__':
         a_limit = a_limit,
         model_folder = model_folder
     )
+    var = 3
     for i in range(episode):
         s = env.reset()
         total_reward = 0
         for j in range(max_iter):
-            env.render()
+            # env.render()
+           
             a = net.chooseAction(s)
-            a = np.clip(np.random.normal(a, 0.5), -2, 2)
+            a = np.clip(np.random.normal(a, var), -2, 2)
             s_, r, finish, info = env.step(a)
+
             net.store_path(s, a, r / 10, s_)
-            net.update()
+
+            if net.memory_counter > net.memory_size:
+                var *= .9995
+                net.update()
+
             s = s_
             total_reward += r
             if j == max_iter - 1:
-                print("Episode: %d \tReward: %i" % (i, total_reward))
+                print("Episode: %d \tReward: %i \t Explore: %.2f \t Pointer: %d" % (i, total_reward, var, net.memory_counter))
                 break
     env.close()
